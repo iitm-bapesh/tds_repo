85f91434ebb0edc9b3d77bcfc756dd925931f081
iitm-bapesh <96167598+iitm-bapesh@users.noreply.github.com>
2022-07-24 14:51:18 +0530

    Update tds_app.py

--- tds_app.py
+++ tds_app.py
@@ -9,8 +9,17 @@ st.write("Please enter 2 two numbers to be subtracted in the below text boxes")
 st.text_input("First Number", key="num1")
 st.text_input("Second Number", key="num2")
 
-st.write(num1)
-st.write(type(num1))
+n1, n2 = None, None
 
-st.write(num2)
-st.write(type(num2))
+if st.session_state.num1:
+  n1 = st.session_state.num1
+  st.write(n1)
+  st.write(type(n1))
+
+if st.session_state.num2:
+  n2 = st.session_state.num2
+  st.write(n2)
+  st.write(type(n2))
+  
+if st.session_state.num1 and st.session_state.num2:
+  st.write("The result of subtracting second number from the first is: "+(n1-n2))
